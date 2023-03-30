94becc5ca3cb61a0fdbda13a2d77d37090405c8a
kmark-n <seasonablesalt@gmail.com>
2023-03-30 06:50:13 -0700

    Square with size and def it as a private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,8 +4,9 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size):
-         """Initialize a new Square.
+        """Initialize a new Square.
         Args:
             size (int): The size of the new square.
         """
