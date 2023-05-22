10e6c3c935320d78baafbb6283cc7a3c7e5b870c
kmark-n <seasonablesalt@gmail.com>
2023-05-22 08:09:58 -0700

    Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Lis all records in the table second_table with a score >= 10.
+--Records are ordered by descending order.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
